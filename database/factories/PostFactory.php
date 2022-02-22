4cc29685ebf07270f9cc88866f614efeac5617f0
Daan de Waard <daan.de.waard@hz.nl>
2022-02-22 08:47:12 +0100

    Fix other codesniffer issue in PostFactory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,7 +18,7 @@ class PostFactory extends Factory
             'excerpt' => $this->faker->paragraph(5),
             'img_url' => 'http://loremflickr.com/800/600?lock=' .
                 $this->faker->numberBetween(1,30976),
-            'body' => '<p>'.implode('</p><p>', $this->faker->paragraphs(5)).'</p>',
+            'body' => '<p>' . implode('</p><p>', $this->faker->paragraphs(5)) . '</p>',
             'published_at' => $this->faker->dateTime
         ];
     }
